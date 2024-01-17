ef90bad79118bee58339f29c61b75ff0e89ef632
Joshuaxyz-dev <58625724+Joshuaxyz-dev@users.noreply.github.com>
2024-01-17 12:05:35 +0100

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fib fib =new Fib();
+        for (int i = 0; i < 10; i++){
+           System.out.println(i);
+        }
+    }
+}
